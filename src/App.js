47bd8abf5caa90bfca45d8bd27ddb7aed564ceb7
Gourav Rana <85408869+GouravRana807@users.noreply.github.com>
2022-10-19 13:00:48 -0800

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, SetData] = useState([]);
+  const getNews = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?q=bitcoin&apiKey=a86c581385b245a2973fcb53c3f96a28"
+      )
+      .then((response) => {
+        console.log(response);
+        SetData(response.data.articles);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-3">
+      <button className="btn btn-primary " onClick={getNews}>
+        Fetch News
+      </button>
+
+      <div className="container">
+        <div className="row">
+          {data.map((value) => {
+            return (
+              <div className="col-3">
+                <div className="card" style={{ width: "18rem" }}>
+                  <img
+
+                    className="card-img-top"
+                    src={value.urlToImage}
+                    alt=""
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{value.tilte}</h5>
+                    <p className="card-text">
+                     {value.description}
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                    Main Nes
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
